d70bd1e7cff188ed8045e416dc38bb69324f3b6d
novakvova <novakvova@gmail>
2023-10-23 18:24:44 +0300

    add activity

--- Project/app/src/main/java/com/example/store/MainActivity.java
+++ Project/app/src/main/java/com/example/store/MainActivity.java
@@ -6,15 +6,28 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText txtValue;
+    private ImageView imgAvatar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txtValue = findViewById(R.id.txtValue);
+        imgAvatar=findViewById(R.id.imgAvatar);
+        String url = "https://kovbasa.itstep.click/images/mala.jpeg";
+        Glide
+                .with(this)
+                .load(url)
+                .apply(new RequestOptions().override(600))
+                .into(imgAvatar);
+
     }
 
     public void onClickMeHandler(View view) {
