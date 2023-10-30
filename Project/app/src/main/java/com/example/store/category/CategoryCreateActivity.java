afade3534d372e625441422be86adf443e4220c6
novakvova <novakvova@gmail>
2023-10-30 20:15:19 +0200

    Select inage

--- Project/app/src/main/java/com/example/store/category/CategoryCreateActivity.java
+++ Project/app/src/main/java/com/example/store/category/CategoryCreateActivity.java
@@ -3,12 +3,18 @@ package com.example.store.category;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.store.CategoriesActivity;
 import com.example.store.R;
+import com.example.store.application.HomeApplication;
 import com.example.store.dto.category.CategoryCreateDTO;
 import com.example.store.dto.category.CategoryItemDTO;
 import com.example.store.services.ApplicationNetwork;
@@ -20,8 +26,10 @@ import retrofit2.Response;
 
 public class CategoryCreateActivity extends AppCompatActivity {
 
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private ImageView ivSelectImage;
     TextInputLayout tlCategoryName;
-    TextInputLayout tlCategoryImage;
+
     TextInputLayout tlCategoryDescription;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,21 +37,28 @@ public class CategoryCreateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_category_create);
 
         tlCategoryName = findViewById(R.id.tlCategoryName);
-        tlCategoryImage = findViewById(R.id.tlCategoryImage);
         tlCategoryDescription = findViewById(R.id.tlCategoryDescription);
+
+        ivSelectImage = findViewById(R.id.ivSelectImage);
+        String url = "https://cdn.pixabay.com/photo/2016/01/03/00/43/upload-1118929_1280.png";
+        Glide
+                .with(HomeApplication.getAppContext())
+                .load(url)
+                .apply(new RequestOptions().override(300))
+                .into(ivSelectImage);
     }
 
     public void onClickCreateCategory(View view)
     {
         String name = tlCategoryName.getEditText().getText().toString().trim();
-        String image = tlCategoryImage.getEditText().getText().toString().trim();
+
         String description = tlCategoryDescription.getEditText().getText().toString().trim();
 //        Log.d("--salo--", "--name--" + name);
 //        Log.d("--salo--", "--image--" + image);
 //        Log.d("--salo--", "--description--" + description);
         CategoryCreateDTO dto = new CategoryCreateDTO();
         dto.setName(name);
-        dto.setImage(image);
+
         dto.setDescription(description);
         ApplicationNetwork
                 .getInstance()
@@ -64,5 +79,43 @@ public class CategoryCreateActivity extends AppCompatActivity {
 
                     }
                 });
+    }
+
+    public void openGallery(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            // Get the URI of the selected image
+            Uri uri = data.getData();
+
+            Glide
+                    .with(HomeApplication.getAppContext())
+                    .load(uri)
+                    .apply(new RequestOptions().override(300))
+                    .into(ivSelectImage);
+
+            // If you want to get the file path from the URI, you can use the following code:
+            //filePath = getPathFromURI(uri);
+        }
+    }
+
+    // This method converts the image URI to the direct file system path of the image file
+    private String getPathFromURI(Uri contentUri) {
+        String[] projection = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(contentUri, projection, null, null, null);
+        if (cursor != null) {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            String filePath = cursor.getString(column_index);
+            cursor.close();
+            return filePath;
+        }
+        return null;
     }
 }
