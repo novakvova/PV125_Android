474ba59f19b7f80d86b501d3ca80706b4518dd87
novakvova <novakvova@gmail>
2023-11-01 19:12:44 +0200

    Test Global is good

--- Project/app/src/main/java/com/example/store/contants/Urls.java
+++ Project/app/src/main/java/com/example/store/contants/Urls.java
@@ -1,6 +1,6 @@
 package com.example.store.contants;
 
 public class Urls {
-//    public static final String BASE="https://kovbasa.itstep.click";
-    public static final String BASE="http://10.0.2.2:5132";
+    public static final String BASE="https://kovbasa.itstep.click";
+//    public static final String BASE="http://10.0.2.2:5132";
 }
