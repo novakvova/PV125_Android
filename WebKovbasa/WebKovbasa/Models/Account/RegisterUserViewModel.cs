3dc944fa95d7dbe258a890eac66efbf09321ef66
novakvova <novakvova@gmail>
2023-11-03 20:20:26 +0200

    Add Model

--- WebKovbasa/WebKovbasa/Models/Account/RegisterUserViewModel.cs
+++ WebKovbasa/WebKovbasa/Models/Account/RegisterUserViewModel.cs
@@ -2,5 +2,10 @@
 {
     public class RegisterUserViewModel
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
     }
 }
