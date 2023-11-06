9efeabbfe126043b3b799fcd48e4ef4b9c1fc66a
novakvova <novakvova@gmail>
2023-11-06 18:39:53 +0200

    Add Image Register

--- WebKovbasa/WebKovbasa/Controllers/AccountController.cs
+++ WebKovbasa/WebKovbasa/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WebKovbasa.Constants;
 using WebKovbasa.Data.Entities.Identity;
+using WebKovbasa.Helpers;
 using WebKovbasa.Models.Account;
 
 namespace WebKovbasa.Controllers
@@ -19,12 +20,20 @@ namespace WebKovbasa.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterUserViewModel model)
         {
+            string imageName = null;
+
+            if (!string.IsNullOrEmpty(model.ImageBase64))
+            {
+                imageName = ImageWorker.SaveImage(model.ImageBase64);
+            }
+
             UserEntity user = new UserEntity()
             {
                 FirstName = model.FirstName,
                 LastName = model.LastName,
                 UserName = model.Email,
-                Email = model.Email
+                Email = model.Email,
+                Image = imageName,
             };
 
             var result = await _userManager.CreateAsync(user, model.Password);
---
+++ WebKovbasa/WebKovbasa/Helpers/ImageWorker.cs
@@ -0,0 +1,59 @@
+﻿using SixLabors.ImageSharp.Formats.Webp;
+
+namespace WebKovbasa.Helpers
+{
+    public static class ImageWorker
+    {
+        public static string SaveImage(string imageBase64)
+        {
+            string fileName = Path.GetRandomFileName() + ".webp";
+            try
+            {
+                string base64 = imageBase64;
+                if (base64.Contains(","))
+                    base64 = base64.Split(',')[1];
+
+                byte[] byteBuffer = Convert.FromBase64String(base64);
+
+
+                string dirSave = Path.Combine(Directory.GetCurrentDirectory(), "images", fileName);
+
+                using (var image = Image.Load(byteBuffer))
+                {
+                    image.Mutate(x =>
+                    {
+                        x.Resize(new ResizeOptions
+                        {
+                            Size = new Size(1200, 1200),
+                            Mode = ResizeMode.Max
+                        });
+                    });
+                    using (var ms = new MemoryStream())
+                    {
+                        image.Save(ms, new WebpEncoder());
+                        System.IO.File.WriteAllBytes(dirSave, ms.ToArray());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Файл не вдалося зберегти!" + ex.Message);
+            }
+            return fileName;
+        }
+
+        public static void RemoveImage(string fileName)
+        {
+            try
+            {
+                string file = Path.Combine(Directory.GetCurrentDirectory(), "images", fileName);
+                if (System.IO.File.Exists(file))
+                {
+                    System.IO.File.Delete(file);
+                }
+            }
+            catch
+            { }
+        }
+    }
+}
